f6635617f9d78ea66174580183b6a7105a54809a
sydyk120276 <sidikjan@mail.ru>
2024-01-24 15:12:27 +0600

    начало

---
+++ src/scripts/modules/swiper.js
@@ -0,0 +1,12 @@
+const sliderThumbs = new Swiper('.lw-promo-catalog-swiper .swiper-wrapper', { 
+	slidesPerView: 3, 
+	spaceBetween: 75,
+	breakpoints: { 
+		0: { 
+
+		},
+		768: { 
+
+		}
+	}
+});
